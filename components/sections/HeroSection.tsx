c2a032c1716d59f35073b6402bded17d3d7f11f7
Shreyansh <kake0shreyansh@gmail.com>
2026-02-10 12:38:22 +0530

    update

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -27,7 +27,7 @@ const itemVariants = {
   visible: {
     y: 0,
     opacity: 1,
-    transition: { duration: 0.9, ease: 'easeOut' }
+    transition: { duration: 0.9, ease: [0.25, 0.1, 0.25, 1] }
   }
 }
 
