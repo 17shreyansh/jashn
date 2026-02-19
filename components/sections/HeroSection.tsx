ec7fc4fe0879c2524a985536938431f95270a97a
Shreyansh <kake0shreyansh@gmail.com>
2026-02-19 10:40:39 +0530

    update

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -27,7 +27,7 @@ const itemVariants = {
     y: 0,
     opacity: 1,
     filter: 'blur(0px)',
-    transition: { duration: 1, ease: [0.22, 1, 0.36, 1] }
+    transition: { duration: 1, ease: [0.22, 1, 0.36, 1] as const }
   }
 }
 
